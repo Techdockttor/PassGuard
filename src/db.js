ca9e3e7f341fc89a91190e2d4ed1c220d71b95f3
NkeJesus Diogu <140277822+Techdockttor@users.noreply.github.com>
2024-06-16 13:20:38 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-
-// Load MongoDB URI from environment variables or use a fallback
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/passguarddb';
+require('dotenv').config(); // Load environment variables from .env file
 
 // Set strictQuery to suppress the warning
 mongoose.set('strictQuery', true); // or false based on your preference
+
+// Access the MongoDB URI from the environment variables
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/passguarddb';
+
+if (!mongoURI) {
+  console.error('Error: MONGODB_URI is not defined in the environment variables.');
+  process.exit(1); // Exit the process with an error code
+}
+
+console.log('MongoURI:', mongoURI); // Debug statement to check the URI
 
 // Set mongoose options and connect to MongoDB
 mongoose.connect(mongoURI, {
