93ab3c795d8b9392497ab37bffc257d13d076d77
NkeJesus Diogu <140277822+Techdockttor@users.noreply.github.com>
2024-06-16 09:55:47 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,8 @@
 const TIMEOUT_SEC = 10;
-const RES_PER_PAGE = 10;
 const KEY = '4efc8bac-cede-4635-998b-a9c572eec747';
-const MODAL_CLOSE_SEC = 2.5;
 
 module.exports = {
   TIMEOUT_SEC,
-  RES_PER_PAGE,
   KEY,
-  MODAL_CLOSE_SEC,
 };
 
