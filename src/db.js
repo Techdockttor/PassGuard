84e9096700509a0b84c7977c3212c961371de598
NkeJesus Diogu <140277822+Techdockttor@users.noreply.github.com>
2024-06-16 12:30:09 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,26 +1,29 @@
 // src/db.js
-const mongoURI = 'mongodb://localhost:27017/passguarddb';
 const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/passguarddb'; 
+require('dotenv').config();
 
 // Set strictQuery to suppress the warning
 mongoose.set('strictQuery', true); // or false based on your preference
 
-mongoose.connect('mongodb://localhost:27017/passguarddb', {
+const mongoURI = process.env.MONGODB_URI;
+
+console.log('MongoURI:', mongoURI); // Debug statement
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+  useCreateIndex: true,
+  useFindAndModify: false
+}).catch(err => console.error('MongoDB connection error:', err));
 
 const db = mongoose.connection;
 
-// Event handlers for MongoDB connection
 db.on('error', (err) => {
   console.error('MongoDB connection error:', err);
 });
 
 db.once('open', () => {
   console.log('Connected to MongoDB');
-  // Perform operations that require a database connection here
 });
 
-// Export the database connection
 module.exports = db;
