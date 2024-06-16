3e7160a5ffe96df18277111a00e82fd5db256818
NkeJesus Diogu <140277822+Techdockttor@users.noreply.github.com>
2024-06-16 11:05:50 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ const authRouter = require('./auth'); // Example router file
 const passwordRouter = require('./passwords');
 const app = express();
 const config = require('./config');
+const { API_URL, KEY } = require('../config.js');
+const { AJAX } = require('../ajax.js');
 
 //CORS
 const corsoptions = {
