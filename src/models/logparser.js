e1859b08952960627271f2724997932687908bbf
NkeJesus Diogu <140277822+Techdockttor@users.noreply.github.com>
2024-06-17 19:02:46 +0100

    Update logparser.js

--- src/models/logparser.js
+++ src/models/logparser.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { API_URL, KEY } = require('../config.js');
+const { AJAX } = require('../ajax.js');
+const mongoURI = 'mongodb://localhost:27017/passguard';
 
 const logEntrySchema = new mongoose.Schema({
   ip: {
