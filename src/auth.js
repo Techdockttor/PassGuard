706e7c78691bb8625e400ab6354bbb9b31e59c6c
NkeJesus Diogu <140277822+Techdockttor@users.noreply.github.com>
2024-06-19 10:37:33 +0100

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/user');
 const Password = require('./models/password');
 const authenticate = require('./authenticate');
-const LogEntry = require('./models/logEntry');
+const LogEntry = require('./models/LogEntry');
 
 // Signup route
 router.post('/signup', async (req, res) => {
