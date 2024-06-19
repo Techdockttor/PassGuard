c50a88b7dad404e79cea06f6a23656a9dcf5bea8
NkeJesus Diogu <140277822+Techdockttor@users.noreply.github.com>
2024-06-19 10:59:21 +0100

    Update authenticate.js

--- src/authenticate.js
+++ src/authenticate.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/user'); // Adjust the path as per your project structure
 const LogEntry = require('./models/LogEntry'); // Adjust the path as per your project structure
 
-const authenticateToken = async (req, res, next) => {
+const authenticate = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1]; // Assuming token is passed in the Authorization header
 
   if (!token) {
